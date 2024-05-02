f263bb8848529a75d0f02b7e97c76199cdcb88d0
RRIVAS2 <ricardorl1990@gmail.com>
2024-05-01 22:19:59 -0600

    update

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,13 +1,4 @@
-// Get the current year
 let currentYear = new Date().getFullYear();
 
-// Set the copyright year in the first paragraph of the footer
-let copyrightParagraph = document.getElementById('currentyear');
-copyrightParagraph.innerHTML = `© ${currentYear} - Ricardo Rivas - San Jose, Costa Rica`;
-
-// Get the last modified date from the document's lastModified property
-let lastModifiedDate = document.lastModified;
-
-// Set the last modified date in the second paragraph of the footer
-const lastModifiedParagraph = document.getElementById('lastModified');
-lastModifiedParagraph.textContent = `Last Modification: ${lastModifiedDate}`;
+document.getElementById("currentYear").innerHTML = currentYear;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
